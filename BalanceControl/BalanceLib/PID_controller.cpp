d1abdd29c8f4e3a62fff310fc88e8523ce712222
Marlene Glawischnig <marlene.glawischnig@edu.fh-joanneum.at>
2018-05-30 15:49:43 +0200

    1. Entwurf PID Regler

--- BalanceControl/BalanceLib/PID_controller.cpp
+++ BalanceControl/BalanceLib/PID_controller.cpp
@@ -2,11 +2,59 @@
 
 
 
-PID_controller::PID_controller()
+PID_controller::PID_controller() :kp_{ 1 }, ki_{ 0 }, kd_{ 0 }
 {
 }
 
-
-PID_controller::~PID_controller()
+PID_controller::PID_controller(double kp, double ki, double kd):kp_{ kp }, ki_{ ki }, kd_{ kd }
 {
+}
+
+PID_controller::get_kp() const
+{
+	return kp_;
+}
+
+PID_controller::get_ki() const
+{
+	return ki_;
+}
+
+PID_controller::get_kd() const
+{
+	return kd_;
+}
+
+PID_controller::set_kp(double kp)
+{ 
+	kp_ = kp;
+}
+
+PID_controller::set_ki(double ki)
+{
+	ki_ = ki;
+}
+
+PID_controller::set_kd(double kd)
+{
+	kd_ = kd;
+}
+
+PID_controller::set_process_value(double y)
+{
+	y_ = y;
+
+}
+
+PID_controller::get_control_value() const
+{
+	return u_;
+
+}
+
+PID_controller::step()
+{
+	u_ = -(kp_ * y_ + ki_* STEPSIZE * y_ + kd_*(y_ - y_prev) / STEPSIZE);
+	y_prev = y_;
+
 }
--- BalanceControl/BalanceLib/PID_controller.h
+++ BalanceControl/BalanceLib/PID_controller.h
@@ -2,7 +2,20 @@
 class PID_controller
 {
 public:
-	PID_controller();
-	~PID_controller();
+	PID_controller(double kp, double ki, double kd);
+	double get_kp() const;
+	double get_ki() const;
+	double get_kd() const;
+	void set_kp(double kp);
+	void set_ki(double ki);
+	void set_kd(double kd);
+	void set_process_value(double value);
+	double get_control_value(double value) const;
+	void step();
+
+private:
+	double p_;
+	double i_;
+	double d_;
 };
 
