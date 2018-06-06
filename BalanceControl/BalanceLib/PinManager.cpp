6616b3173239bda2bd3baea6ba839807655a22c6
Netta Nona <nona.netta@edu.fh-joanneum.at>
2018-06-06 11:08:28 +0200

    Pinmanager und Konfiguration hinzugefügt

---
+++ BalanceControl/BalanceLib/PinManager.cpp
@@ -0,0 +1,22 @@
+#include "PinManager.h"
+
+Pin_manager::Pin_manager() {
+}
+
+int Pin_manager::reserve(int pin) {
+	std::vector<int>::iterator position = std::find(reserved_pins_.begin(), reserved_pins_.end(), pin);
+	if (position != reserved_pins_.end()) {
+		throw std::runtime_error("Pin was already reserved!");
+		return -1;
+	} else {
+		reserved_pins_.push_back(pin);
+		return pin;
+	}
+}
+
+void Pin_manager::release(int pin) {
+	std::vector<int>::iterator position = std::find(reserved_pins_.begin(), reserved_pins_.end(), pin);
+	if (position != reserved_pins_.end())
+		reserved_pins_.erase(position);
+	else  throw std::runtime_error("Pin was not reserved!");
+}
---
+++ BalanceControl/BalanceLib/PinManager.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <vector>
+#include <stdexcept>
+#include <algorithm>
+
+class Pin_manager {
+private:
+	static std::vector<int> reserved_pins_;
+public:
+	Pin_manager();
+	int reserve(int pin);
+	void release(int pin);
+};
