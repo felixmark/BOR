31e9617e5147b7db72095104448b2c62c780917b
Felix Mark <felix.mark@hotmail.com>
2018-06-10 10:31:40 +0200

    Pi korrigiert (12. Nachkommastelle oder so)

--- BalanceControl/BalanceLib/Global_definitions.h
+++ BalanceControl/BalanceLib/Global_definitions.h
@@ -3,5 +3,5 @@
 // Global Definitions for all modules
 
 constexpr double STEPSIZE = 0.001;					// Simulation stepsize in SECONDS
-constexpr double PI = 3.14159265358979323746264;	// Pi
+constexpr double PI = 3.1415926535897932384626433;	// Pi
 constexpr double GRAVITY = 9.80665;					// Gravity constant g
