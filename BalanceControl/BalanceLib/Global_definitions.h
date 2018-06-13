734207f7b51c52ae21c147a18d6267e4b60d8ae1
Felix Mark <felix.mark@hotmail.com>
2018-06-13 16:56:15 +0200

    global definitions update

--- BalanceControl/BalanceLib/Global_definitions.h
+++ BalanceControl/BalanceLib/Global_definitions.h
@@ -5,3 +5,5 @@
 constexpr double STEPSIZE = 0.001;					// Simulation stepsize in SECONDS
 constexpr double PI = 3.1415926535897932384626433;	// Pi
 constexpr double GRAVITY = 9.80665;					// Gravity constant g
+
+double grad_to_rad(double grad);
